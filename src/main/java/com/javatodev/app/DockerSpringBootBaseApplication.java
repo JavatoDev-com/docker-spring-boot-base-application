93a2bf6cff6cfe01ced826106d812dad1e401daf
Chinthaka Dinadasa <sendtoclabz@gmail.com>
2023-04-15 00:24:02 +0530

    init application

---
+++ src/main/java/com/javatodev/app/DockerSpringBootBaseApplication.java
@@ -0,0 +1,13 @@
+package com.javatodev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringBootBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerSpringBootBaseApplication.class, args);
+    }
+
+}
